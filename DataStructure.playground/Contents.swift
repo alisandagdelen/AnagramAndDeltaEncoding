f8f1cf219d65f246f0d5b43a206e5286a1fe3877
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-02-18 19:17:39 +0300

    - Anagram and DeltaEncoding

---
+++ DataStructure.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Data Structure
+
+import UIKit
+
+// 1- Anagrams
+typealias dictStringArray = [String:[String]]
+
+func findAnagrams(words:[String]) -> dictStringArray {
+    var anagrams:dictStringArray = [:]
+    
+    for word in words {
+        let formatedWord = word.replacingOccurrences(of: " ", with: "").lowercased().sorted()
+        let formatedString = String(formatedWord)
+        
+        if let _ = anagrams[formatedString] {
+            anagrams[formatedString]?.append(word)
+        } else {
+            anagrams[formatedString] = [word]
+        }
+    }
+    return anagrams
+}
+
+// Anagram test
+let anagrams = findAnagrams(words: ["pear", "amleth", "dormitory", "tinsel", "dirty", "room", "hamlet", "listen", "silent"])
+for anagram in anagrams {
+    print(anagram.value)
+}
+
+// 2- Delta encoding
+
+func deltaEncode(elements:[Int],escapeToken:Int) -> [Int]{
+    var encodedElements:[Int] = []
+    
+    for (index, element) in elements.enumerated() {
+        if index == 0 {
+            encodedElements.append(element)
+            continue
+        }
+        let diffrence = element - elements[index - 1]
+        if diffrence <= -127 || diffrence >= 127 {
+            encodedElements.append(contentsOf: [escapeToken, diffrence])
+        } else {
+            encodedElements.append(diffrence)
+        }
+    }
+    return encodedElements
+}
+
+let elements = [25626, 25757, 24367, 24267, 16, 100, 2, 7277]
+print(deltaEncode(elements: elements, escapeToken: -128))
+
